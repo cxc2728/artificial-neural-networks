66e27db1941201ba37d143f334428d2a6001496e
Carlo Ciulla <cxc2728@njit.edu>
2025-12-24 07:59:51 +0100

    Add bias weights and update weight saving logic
    Added bias weights to the neural network and updated the logic for saving weights.

--- net2025v2.cpp
+++ net2025v2.cpp
@@ -486,7 +486,9 @@ int main(int argc, char* argv[]) {
 			fwrite(&w2, sizeof(double), 1, saveWeights);
 			fwrite(&w3, sizeof(double), 1, saveWeights);
 			fwrite(&w4, sizeof(double), 1, saveWeights);
-	
+	        fwrite(&b1, sizeof(double), 1, saveWeights);
+			fwrite(&b2, sizeof(double), 1, saveWeights);
+
 			fclose(saveWeights);
 
 	} // (save)
